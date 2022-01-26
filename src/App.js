2d5e5595efa055f1e42e28866a3796e1e3cb37f7
Aditya Karki <adityakarki728@gmail.com>
2022-01-26 12:34:03 +0545

    Added necessary files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import Feed from './components/Feed/Feed';
+import Header from './components/Header/Header';
+import Login from './components/Login/Login';
+import Sidebar from './components/Sidebar/Sidebar';
+import Widgets from './components/Widgets/Widgets';
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+
   return (
-    //BEM naming convention
-    <div className="App">
-      <h1>Lets build Facebook</h1>
+    <div className="app">
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </>
+      )}
     </div>
   );
 }
